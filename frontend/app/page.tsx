1aae69930358cb33f796672b1a47a28613bf249d
Dinh Tan <dinhtan1104@gmail.com>
2026-02-26 23:28:11 +0100

    feat: bandeau CTA avec choix Uber Eats et Deliveroo

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -2,6 +2,7 @@ import LandingHeader from '@/app/components/LandingHeader'
 import ImageCarousel from '@/app/components/ImageCarousel'
 import HeroSection from '@/app/components/HeroSection'
 import Engagements from '@/app/components/Engagements'
+import CTABanner from '@/app/components/CTABanner'
 import Footer from '@/app/components/Footer'
 
 export default function Page() {
@@ -18,6 +19,9 @@ export default function Page() {
 
       {/* Carousel Section */}
       <ImageCarousel />
+
+      {/* Bandeau CTA */}
+      <CTABanner />
 
       {/* Footer */}
       <Footer />
